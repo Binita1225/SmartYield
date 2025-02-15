3736afcc54468a74aa252ec676a6905b0726cdb0
Prajwal Baral <prajwalbaral001@gmail.com>
2025-02-15 17:04:53 +0545

    admin page download files created

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
-// app/admin/page.tsx
-"use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import PrimaryButton from "@/components/UI/PrimaryButton";
+import SecondaryButton from "@/components/UI/SecondaryButton";
 
 const AdminPage = () => {
   const router = useRouter();
@@ -9,31 +9,70 @@ const AdminPage = () => {
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("userData") || "{}");
-
-    // You don't need client-side redirects here, it's handled by the middleware
     if (
       !userData.role ||
       (userData.role !== "Admin" && userData.role !== "SuperAdmin")
     ) {
-      setIsAuthorized(false); // Optionally, show an unauthorized message
-      router.push("/"); // Redirect if user is unauthorized
+      setIsAuthorized(false);
+      router.push("/");
     } else {
       setIsAuthorized(true);
     }
   }, []);
 
   if (isAuthorized === null) {
-    return <p>Loading...</p>; // Show loading state while checking authorization
+    return <p>Loading...</p>;
   }
 
   if (!isAuthorized) {
-    return <p>You are not authorized to access this page.</p>; // Show unauthorized message
+    return <p>You are not authorized to access this page.</p>;
   }
 
+  const handleDatasetDownload = () => {
+    const datasetUrl = "/public/Dataset.csv";
+    const a = document.createElement("a");
+    a.href = datasetUrl;
+    a.download = "dataset.zip";
+    a.click();
+  };
+  const handleDocumnetDownload = () => {
+    const datasetUrl = "/public/Document.pdf";
+    const a = document.createElement("a");
+    a.href = datasetUrl;
+    a.download = "dataset.zip";
+    a.click();
+  };
+  const handlemodalcode = () => {
+    const datasetUrl = "/public/Model.py";
+    const a = document.createElement("a");
+    a.href = datasetUrl;
+    a.download = "dataset.zip";
+    a.click();
+  };
+
   return (
-    <div>
-      <h1>Welcome to the Admin Page</h1>
-      {/* Your admin page content here */}
+    <div className="container mx-auto">
+      <div className="grid grid-cols-2 items-center gap-10">
+        <div>
+          <h3 className="text-[3rem] tracking-[-2.72px] leading-[4.5rem] text-center font-[500] __className_4bc053">
+            Our Sample Snapshot
+          </h3>
+          <img src="/datasetimage" width="100%" />
+        </div>
+        <div>
+          <div className="flex flex-wrap items-center gap-10">
+            <span onClick={handleDatasetDownload}>
+              <PrimaryButton name="Download Dataset" />
+            </span>
+            <span onClick={handleDocumnetDownload}>
+              <SecondaryButton name="Download Documentation" />
+            </span>
+            <span  onClick={handlemodalcode} >
+            <PrimaryButton name="Download Modal" />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
